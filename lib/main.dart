ff6dd9f3a3215c29ae80f135f031cf7bd1200966
cdemy Leitung <88116396+cdemyLeitung@users.noreply.github.com>
2022-01-10 12:25:49 +0100

    x

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -38,94 +40,205 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Bla'),
       ),
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          final maxX = constraints.maxWidth / 3;
-          final maxY = constraints.maxHeight / 4;
-          final max = maxX > maxY ? maxY : maxX;
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                color: Colors.black,
-                width: double.infinity,
-                height: maxY,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Taste(
-                    max: max,
-                    color: Colors.yellowAccent,
-                  ),
-                  Taste(
-                    max: max,
-                    color: Colors.brown,
-                  ),
-                  Taste(
-                    max: max,
-                    color: Colors.green,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Taste(
-                    max: max,
-                    color: Colors.brown,
-                  ),
-                  Taste(
-                    max: max,
-                    color: Colors.green,
-                  ),
-                  Taste(
-                    max: max,
-                    color: Colors.yellowAccent,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Taste(
-                    max: max,
-                    color: Colors.green,
-                  ),
-                  Taste(
-                    max: max,
-                    color: Colors.yellowAccent,
-                  ),
-                  Taste(
-                    max: max,
-                    color: Colors.brown,
-                  ),
-                ],
-              ),
-            ],
-          );
-        },
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height - AppBar().preferredSize.height,
+        child: OrientationBuilder(
+          builder: (context, orientation) => orientation == Orientation.portrait
+              ? Column(
+                  children: [
+                    RaumReihe(
+                      children: [
+                        RaumZwischen(),
+                        RaumEingabe(flex: 32),
+                        RaumZwischen(),
+                      ],
+                    ),
+                    RaumZwischen(),
+                    RaumReihe(
+                      children: [
+                        RaumZwischen(),
+                        RaumTaste(title: '1'),
+                        RaumZwischen(),
+                        RaumTaste(title: '2'),
+                        RaumZwischen(),
+                        RaumTaste(title: '3'),
+                        RaumZwischen(),
+                      ],
+                    ),
+                    RaumZwischen(),
+                    RaumReihe(
+                      children: [
+                        RaumZwischen(),
+                        RaumTaste(title: '4'),
+                        RaumZwischen(),
+                        RaumTaste(title: '5'),
+                        RaumZwischen(),
+                        RaumTaste(title: '6'),
+                        RaumZwischen(),
+                      ],
+                    ),
+                    RaumZwischen(),
+                    RaumReihe(
+                      children: [
+                        RaumZwischen(),
+                        RaumTaste(title: '7'),
+                        RaumZwischen(),
+                        RaumTaste(title: '8'),
+                        RaumZwischen(),
+                        RaumTaste(title: '9'),
+                        RaumZwischen(),
+                      ],
+                    ),
+                    RaumZwischen(),
+                    RaumReihe(
+                      children: [
+                        RaumZwischen(),
+                        RaumTaste(title: 'DEL'),
+                        RaumZwischen(),
+                        RaumTaste(title: '0'),
+                        RaumZwischen(),
+                        RaumTaste(title: 'OK'),
+                        RaumZwischen(),
+                      ],
+                    ),
+                    RaumZwischen(),
+                  ],
+                )
+              : Column(
+                  children: [
+                    RaumReihe(
+                      children: [
+                        RaumZwischen(),
+                        RaumTaste(title: 'DEL'),
+                        RaumZwischen(),
+                        RaumEingabe(flex: 32),
+                        RaumZwischen(),
+                        RaumTaste(title: 'OK'),
+                        RaumZwischen(),
+                      ],
+                    ),
+                    RaumZwischen(),
+                    RaumReihe(
+                      children: [
+                        RaumZwischen(),
+                        RaumTaste(title: '1'),
+                        RaumZwischen(),
+                        RaumTaste(title: '2'),
+                        RaumZwischen(),
+                        RaumTaste(title: '3'),
+                        RaumZwischen(),
+                        RaumTaste(title: '4'),
+                        RaumZwischen(),
+                        RaumTaste(title: '5'),
+                        RaumZwischen(),
+                      ],
+                    ),
+                    RaumZwischen(),
+                    RaumReihe(
+                      children: [
+                        RaumZwischen(),
+                        RaumTaste(title: '6'),
+                        RaumZwischen(),
+                        RaumTaste(title: '7'),
+                        RaumZwischen(),
+                        RaumTaste(title: '8'),
+                        RaumZwischen(),
+                        RaumTaste(title: '9'),
+                        RaumZwischen(),
+                        RaumTaste(title: '0'),
+                        RaumZwischen(),
+                      ],
+                    ),
+                    RaumZwischen(),
+                  ],
+                ),
+        ),
       ),
     );
   }
 }
 
-class Taste extends StatelessWidget {
-  const Taste({
+class RaumZwischen extends StatelessWidget {
+  const RaumZwischen({
     Key? key,
-    required this.color,
-    required this.max,
   }) : super(key: key);
-
-  final double max;
-  final Color color;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: color,
-      width: max,
-      height: max,
+    return Expanded(child: Container(color: Colors.yellow));
+  }
+}
+
+class RaumReihe extends StatelessWidget {
+  final List<Widget> children;
+  const RaumReihe({
+    this.children = const <Widget>[],
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 10,
+      child: Container(
+        color: Colors.red,
+        child: Row(
+          children: [...children],
+        ),
+      ),
+    );
+  }
+}
+
+class RaumTaste extends StatelessWidget {
+  final String title;
+  const RaumTaste({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 10,
+      child: LayoutBuilder(
+        builder: (context, constraints) => Container(
+          color: Colors.blue,
+          child: Text(title),
+          height: (constraints.maxHeight > constraints.maxWidth) ? constraints.maxWidth : constraints.maxHeight,
+          width: (constraints.maxHeight > constraints.maxWidth) ? constraints.maxWidth : constraints.maxHeight,
+        ),
+      ),
+    );
+  }
+}
+
+class RaumEingabe extends StatelessWidget {
+  final int flex;
+  const RaumEingabe({
+    required this.flex,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: flex,
+      child: LayoutBuilder(builder: (context, constraints) {
+        final maxHeight = constraints.maxHeight > constraints.maxWidth / (flex / 10.0)
+            ? constraints.maxWidth / (flex / 10.0)
+            : constraints.maxHeight;
+        final maxWidth = constraints.maxHeight > constraints.maxWidth / (flex / 10.0)
+            ? constraints.maxWidth
+            : constraints.maxHeight * (flex / 10.0);
+        return Container(
+          color: Colors.grey,
+          child: const Text('Eingabe'),
+          height: maxHeight,
+          width: maxWidth,
+        );
+      }),
     );
   }
 }
